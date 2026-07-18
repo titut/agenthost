f2f621de048067f632234dc87119945282e779e6
Bill Le <choangbill.le@gmail.com>
2026-07-18 23:20:34 +0700

    remove laptop agent

--- laptop_agent/prepare_qwen_image.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-
-# 1. Load your screenshot (any resolution; it will be stretched to a square)
-img = Image.open("screenshot.png")
-
-# 2. Force-stretch it to a 1024x1024 square (No padding!)
-squared_img = img.resize((1024, 1024), Image.Resampling.LANCZOS)
-squared_img.save("ready_for_qwen.png")
